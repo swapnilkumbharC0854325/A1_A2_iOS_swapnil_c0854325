4b8a498d319b151168da7a056857767270830bb9
Swapkumbhar31 <swapkumbhar31@gmail.com>
2022-05-24 18:18:14 -0400

    Initial Commit

--- Maps/ViewController.swift
+++ Maps/ViewController.swift
@@ -6,14 +6,28 @@
 //
 
 import UIKit
+import CoreLocation
 
 class ViewController: UIViewController {
+    
+    var locationManager = CLLocationManager();
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+        locationManager.delegate = self;
+        
+        locationManager.requestWhenInUseAuthorization();
+        
+        
     }
 
 
 }
 
+
+extension ViewController: CLLocationManagerDelegate {
+    
+}
