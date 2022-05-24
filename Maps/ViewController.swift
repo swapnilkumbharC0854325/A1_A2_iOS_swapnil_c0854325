1442eb165abf92e1edc5a162401f46841ca153f3
Swapkumbhar31 <swapkumbhar31@gmail.com>
2022-05-24 18:48:19 -0400

    displayed user location

--- Maps/ViewController.swift
+++ Maps/ViewController.swift
@@ -7,9 +7,11 @@
 
 import UIKit
 import CoreLocation
+import MapKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var mapView: MKMapView!
     var locationManager = CLLocationManager();
 
     override func viewDidLoad() {
@@ -21,13 +23,47 @@ class ViewController: UIViewController {
         
         locationManager.requestWhenInUseAuthorization();
         
+        locationManager.startUpdatingLocation();
         
     }
-
-
+    
+    func displayPointer(
+        lat: CLLocationDegrees,
+        lng: CLLocationDegrees
+    ) {
+        let latDelta: CLLocationDegrees = 0.05;
+        let lngDelta: CLLocationDegrees = 0.05;
+        
+        let span = MKCoordinateSpan(latitudeDelta: latDelta, longitudeDelta: lngDelta);
+        
+        let location = CLLocationCoordinate2D(latitude: lat, longitude: lng)
+        
+        let region = MKCoordinateRegion(center: location, span: span)
+        
+        
+        mapView.setRegion(region, animated: true)
+        addAnnotation(coordinate: location, title: "My Location")
+        
+    }
+    
+    func addAnnotation(
+        coordinate: CLLocationCoordinate2D,
+        title: String
+    ) {
+        let annotation = MKPointAnnotation();
+        annotation.title = title;
+        annotation.coordinate = coordinate;
+        mapView.addAnnotation(annotation);
+    }
 }
 
 
 extension ViewController: CLLocationManagerDelegate {
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location: CLLocation = locations.last {
+            displayPointer(lat: location.coordinate.latitude, lng: location.coordinate.longitude)
+        }
+    }
     
 }
